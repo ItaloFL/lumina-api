4aec5f69ce6b3c96aba5c2fc11e4d3bd048277e1
ItaloFL <grd788@gmail.com>
2025-12-08 16:50:10 -0500

    fix :bug: cookie token another user

--- src/modules/auth/logout/user-logout-controller.ts
+++ src/modules/auth/logout/user-logout-controller.ts
@@ -4,8 +4,8 @@ export class UserLogoutController {
   async handle(request: Request, response: Response) {
     response.clearCookie("token", {
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
     });
 
     return response.status(200).json({ message: "Logout realizado" });
