0d4ed6acb96fa3e7eb90f83f07d0824f0669a9cc
ItaloFL <grd788@gmail.com>
2025-12-08 15:00:04 -0500

    refactor :recycle: auth google secret and cookie config

--- src/lib/google-auth.ts
+++ src/lib/google-auth.ts
@@ -4,5 +4,5 @@ import { env } from "../env";
 export const oAuth2Client = new OAuth2Client({
   client_id: env.GOOGLE_CLIENT_ID,
   client_secret: env.GOOGLE_CLIENT_SECRET,
-  redirectUri: "http://localhost:3333/auth/google/callback",
+  redirectUri: `${env.BASE_APP_URL}/auth/google/callback`,
 });
