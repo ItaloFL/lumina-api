9c95e765d5e302244e712febde4b04bcd1daa28e
ItaloFL <grd788@gmail.com>
2025-12-10 17:49:17 -0500

    refactor :recycle: lint on generate google auth url

--- src/modules/auth/google/generate-google-auth-url/generate-google-auth-url-controller.ts
+++ src/modules/auth/google/generate-google-auth-url/generate-google-auth-url-controller.ts
@@ -1,15 +1,12 @@
 import { Request, Response } from "express";
 import { GenerateGoogleAuthUrlUseCase } from "./generate-google-auth-url-usecase";
 
-
-
 export class GenerateGoogleAuthUrlController {
-  async handle(request: Request, response: Response){
+  async handle(request: Request, response: Response) {
+    const generateGoogleAuthUrlUseCase = new GenerateGoogleAuthUrlUseCase();
 
-    const generateGoogleAuthUrlUseCase = new GenerateGoogleAuthUrlUseCase()
+    const url = await generateGoogleAuthUrlUseCase.execute();
 
-    const url = await generateGoogleAuthUrlUseCase.execute()
-
-    return response.redirect(url)
+    return response.redirect(url);
   }
 }
